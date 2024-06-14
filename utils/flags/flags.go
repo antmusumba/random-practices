46afcb9620411e2c1a975f0872ad32f8f524a337
antmusumba <musumbaantony3@gmail.com>
2024-06-14 11:50:57 +0300

    feat: add a program that illustrates simple flag defination

--- utils/flags/flags.go
+++ utils/flags/flags.go
@@ -3,11 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
-func main() {
-	// declare a pointer variable
-	ColorPtr := flag.String("color", "blue", "clouring output")
+func main(){
+	// usage: go run . --color=blue green
+	if len(os.Args[1:]) < 1 {
+		fmt.Println("provide argument")
+		return
+	}
+	var ColorPtr =flag.String("color","blue","my color")
 	flag.Parse()
-	fmt.Println(*ColorPtr)
+	fmt.Println("the color parsed from the command line is : ",*ColorPtr)
+
+	arg := flag.Args()
+	fmt.Println("the argument exactly after the flag argument is : ",arg[0])
+	
+
 }
